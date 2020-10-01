26e5bfcde5a3ec04e0f4793c9654edab91815f0a
Ron Northcutt <rlnorthcutt@gmail.com>
2020-10-01 15:05:16 -0500

    fix for acf_core event listener on checkout button temporary fix)

--- modules/acf_components/components/libraries/acf_core/acf_core.js
+++ modules/acf_components/components/libraries/acf_core/acf_core.js
@@ -1,8 +1,8 @@
 /**
  * Managing the core ACF functionality.
- * 
+ *
  * We use the global ACF object to manage the state of the system across all
- *  components. This is an assumption we rely on for consistency. The global 
+ *  components. This is an assumption we rely on for consistency. The global
  *  object will contain all of the most recent information, and then it will
  *  be copied to storage for retrieval on the next page load.
  */
@@ -20,4 +20,8 @@ ACF.cart = ACF.cartManager.get();
 
 // Temporary fix for the demo - event listener for checkout buttons. These will
 //  this will be added as a component later.
-document.querySelector('a.coh-style-checkout').addEventListener('click', () => ACF.cartManager.empty());
+let checkoutBtn = document.querySelector('a.coh-style-checkout')
+if (checkoutBtn) {
+  checkoutBtn.addEventListener('click', () => ACF.cartManager.empty());
+}
+
